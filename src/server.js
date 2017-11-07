cfed7bf248f72ed43a14d05b74803b2333190aba
Kyle Brantley <KylePhoneDial@gmail.com>
2017-11-06 19:55:03 -0600

    Fixed ticks wrapping template strings

--- src/server.js
+++ src/server.js
@@ -7,25 +7,20 @@ var io = socketio.listen(config.port);
 
 io.sockets.on('connection', function (socket) {
 
-	//create a new chatroom
-	socket.on('create-room', function (room, username) {
-		socket.join(room);
-		socket.broadcast.to(room).emit('join-room', '${getTimestamp()}: ${username} has joined the chat.');
-	});
-
 	socket.on('join-room', function (room, username) {
 		socket.join(room);
-		socket.broadcast.to(room).emit('join-room', '${getTimestamp()}: ${username} has joined the chat.');
+		socket.broadcast.to(room).emit('join-room', `${getTimestamp()}: ${username} has joined the chat.`);
 	});
 
-	socket.on('leave-room', function (room, username) {
+	socket.on('leave-room', function (username) {
 		socket.leave(room, function(err) {
 			console.log(err);
 		});
+
 	});
 
-	socket.on('send', function (room, username, message) {
-		io.in(room).emit('message', "${getTimestamp} - ${username}: ${message}");
+	socket.on('chat', function (room, username, message) {
+		io.in(room).emit('message', `${getTimestamp} - ${username}: ${message}`);
 	});
 
 });
